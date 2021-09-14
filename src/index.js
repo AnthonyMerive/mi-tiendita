07b675fead15b46feab73bc8bbd637e027ab336f
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2021-09-14 18:15:15 -0500

    logica CRUD finalizada

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Admin from './components/Admin'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Admin />
   </React.StrictMode>,
   document.getElementById('root')
 );
