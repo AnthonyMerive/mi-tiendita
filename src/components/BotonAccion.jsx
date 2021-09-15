a5d005991bf5cf90a56270545b209fdca23df38b
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2021-09-14 21:39:29 -0500

    anthony

--- src/components/BotonAccion.jsx
+++ src/components/BotonAccion.jsx
@@ -28,15 +28,15 @@ export default function BotonAccion() {
         <>
         {
             dataUser[0].email === "admin@example.com" &&
-            <div class="fixed-action-btn">
-                <a class="btn-floating btn-large red">
-                    <i class="large material-icons">mode_edit</i>
-                </a>
+            <div className="fixed-action-btn">
+                <span className="btn-floating btn-large red">
+                    <i className="large material-icons">mode_edit</i>
+                </span>
                 <ul>
-                    <li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>
-                    <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
-                    <li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
-                    <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
+                    <li><span className="btn-floating red"><i className="material-icons">insert_chart</i></span></li>
+                    <li><span className="btn-floating yellow darken-1"><i className="material-icons">format_quote</i></span></li>
+                    <li><span className="btn-floating green"><i className="material-icons">publish</i></span></li>
+                    <li><span className="btn-floating blue"><i className="material-icons">attach_file</i></span></li>
                 </ul>
             </div> 
         }
